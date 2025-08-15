41ff70732ea954489c50707461c85e7c21dd9c80
Ajay kumar Yadav <akyadav4710@gmail.com>
2025-08-16 01:55:17 +0530

    implemented config security filter

--- springSecurity/src/main/java/com/learning/springSecurity/controller/StudentContoller.java
+++ springSecurity/src/main/java/com/learning/springSecurity/controller/StudentContoller.java
@@ -1,14 +1,40 @@
 package com.learning.springSecurity.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.springSecurity.model.Student;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class StudentContoller {
 
-	public void dummyStudent() {
-		Student s1 = new Student(1,"Vishal","K. V. 39 GTC");
+	private List<Student> listOfStudents = new ArrayList<>(
+			List.of(new Student(1, "Vishal", "K. V. 39 GTC"), new Student(2, "Subodh", "Army Public School")));
+
+	@GetMapping("/students")
+	public List<Student> getStudentsList(){
+		return listOfStudents;
+	}
+	
+	
+	@GetMapping("/getCSRFtoken")
+	public CsrfToken getCSRFtoken(HttpServletRequest Request) {
+		return (CsrfToken) Request.getAttribute("_csrf");
+	}
+	
+	@PostMapping("/addStudent")
+	public List<Student> addStudent(@RequestBody Student student) {
+		listOfStudents.add(student);
+		return listOfStudents;
 	}
 	
 }
